d2942b3036ce3b2ec53b6ded71dda7d9d29c063d
chiaragiudici <chiara.giudici6@studenti.unimi.it>
2021-11-02 13:13:47 +0100

    Initial Commit.

---
+++ af1/src/main/java/Utenti/Elettore.java
@@ -0,0 +1,32 @@
+package Utenti;
+
+public class Elettore {
+	private String nome, cognome;
+	private String docIdent;
+	private String tessElett;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getDocIdent() {
+		return docIdent;
+	}
+	public void setDocIdent(String docIdent) {
+		this.docIdent = docIdent;
+	}
+	public String getTessElett() {
+		return tessElett;
+	}
+	public void setTessElett(String tessElett) {
+		this.tessElett = tessElett;
+	}
+	
+}
---
+++ af1/src/main/java/Utenti/Scrutatore.java
@@ -0,0 +1,25 @@
+package Utenti;
+
+public class Scrutatore {
+	private String nome, cognome;
+	private String docIdent;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getDocIdent() {
+		return docIdent;
+	}
+	public void setDocIdent(String docIdent) {
+		this.docIdent = docIdent;
+	}
+	
+}
