51de97ce035231c7a7db05e1e99ae595f615fe48
chiaragiudici <chiara.giudici6@studenti.unimi.it>
2021-11-04 18:41:13 +0100

    Inseriti possibili metodi per le classi Scrutatore ed Elettore.

--- af1/src/main/java/Utenti/Elettore.java
+++ af1/src/main/java/Utenti/Elettore.java
@@ -29,5 +29,31 @@ public class Elettore {
 	public void setTessElett(String tessElett) {
 		this.tessElett = tessElett;
 	}
-	
+	public void visualizzaCandidati(){
+		//todo l'elettore deve poter visualizzare la lista di candidati
+	}
+	public void visualizzaQuesito(){
+		//todo l'elettore deve poter visualizzare il quesito del referendum
+	}
+	public void VotoCategorico(){
+		//todo l'elettore deve poter inserire un voto, ma anche restituire scheda bianca
+	}
+	public void votoOrdinale(){
+		//todo l'elettore deve poter riordinare la lista di candidati, ma anche restituire scheda bianca
+	}
+	public void votoCategoricoPreferenze(){
+		//todo l'elettore deve poter fino a due voti secondo i requisiti, ma anche restituire scheda bianca
+	}
+	public void referendum(){
+		//todo l'elettore deve poter inserire un voto positivo o negativo al quesito, ma anche restituire scheda bianca
+	}
+	public void confermaScelta(){
+		//todo l'elettore deve poter confermare il proprio voto oppure rifarlo
+	}
+	public void modalitaVittoria(){
+		//todo l'elettore deve poter visualizzare la modalità di calcolo del vincitore / della decisione
+	}
+	public void visualizzaRisultato(){
+		//todo l'elettore deve poter visualizzare il vincitore / la decisione vincente
+	}
 }
--- af1/src/main/java/Utenti/Scrutatore.java
+++ af1/src/main/java/Utenti/Scrutatore.java
@@ -23,4 +23,55 @@ public class Scrutatore {
 		this.docIdent = docIdent;
 	}
 	
+	public void apriVotazione() {
+		//todo lo scrutatore deve poter aprire una sessione di votazione
+	}
+
+	public void sceltaModalita() {
+		//todo lo scrutatore deve poter scegliere la modalità di votazione
+	}
+	
+	public void inserimentoListe() {
+		//todo lo scrutatore deve poter inserire le liste dei candidati
+	}
+
+	public void inserimentoQuesito() {
+		//todo lo scrutatore deve poter inserire il quesito del referendum
+	}
+
+	public void chiudiVotazione() {
+		//todo lo scrutatore deve poter chiudere una sessione di votazione
+	}
+	
+	public void apriScrutinio() {
+		//todo lo scrutatore deve poter aprire una sessione di scrutinio
+	}
+	
+	public void modalitaVittoria() {
+		//todo lo scrutatore deve poter inserire la modalità di vittoria
+	}
+
+	public void visualizzaNumeroVoti() {
+		//todo lo scrutatore deve poter visualizzare il numero degli elettori che hanno votato
+	}	
+	
+	public void visualizzaSchede() {
+		//todo lo scrutatore deve poter visualizzare le schede elettorali
+	}	
+	
+	public void chiudiScrutinio() {
+		//todo lo scrutatore deve poter chiudere una sessione di scrutinio
+	}
+
+	public void apriRisultato() {
+		//todo lo scrutatore deve poter aprire una sessione di visualizzazione del vincitore
+	}
+	
+	public void inserisciRisultato() {
+		//todo lo scrutatore deve poter inserire il candidato/risultato vincitore
+	}	
+	
+	public void chiudiRisultato() {
+		//todo lo scrutatore deve poter chiudere una sessione di visualizzazione del vincitore
+	}
 }
